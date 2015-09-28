fbe265b3360d6cfbf2720b564f03ce579ad7eb2a
trueheartedly <trueheartedly@gmail.com>
2015-09-27 21:13:59 -0700

    near final

--- Carousel/SignInViewController.swift
+++ Carousel/SignInViewController.swift
@@ -17,12 +17,34 @@ class SignInViewController: UIViewController {
         navigationController!.popViewControllerAnimated(true)
     }
     
+    @IBOutlet weak var formContainer: UIView!
+    
+    var initialY: CGFloat!
+    let offset: CGFloat = -100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initialY = formContainer.frame.origin.y
         
-        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
+    
+    @IBAction func didPressBackground(sender: AnyObject) {
+        formContainer.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        
+        self.formContainer.frame.origin = CGPoint(x: self.formContainer.frame.origin.x, y: self.initialY + self.offset)
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        
+        self.formContainer.frame.origin = CGPoint(x: self.formContainer.frame.origin.x, y: self.initialY)
+    }
+    
     
     @IBAction func didPressSignIn(sender: AnyObject) {
         
